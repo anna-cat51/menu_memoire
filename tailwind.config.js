0e85e48d5c596b149135234e1ef5eede0f78b16d
AnnaDoi <willow6510@yahoo.co.jp>
2023-11-14 21:59:39 +0900

    add: daisyUIインストール

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,8 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ]
+  plugins: [require("daisyui")],
 }
