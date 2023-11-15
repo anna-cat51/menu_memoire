da2610353245d72d00d49de07ff5cf2e4f073ccf
AnnaDoi <willow6510@yahoo.co.jp>
2023-11-15 12:12:25 +0900

    fix: 背景色の変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
-    '../myapp/app/views/**/*.html.erb',
-    '../myapp/app/helpers/**/*.rb',
-    '../myapp/app/assets/stylesheets/**/*.css',
-    '../myapp/app/javascript/**/*.js'
+    "../myapp/app/views/**/*.html.erb",
+    "../myapp/app/helpers/**/*.rb",
+    "../myapp/app/assets/stylesheets/**/*.css",
+    "../myapp/app/javascript/**/*.js",
   ],
-  plugins: [require('daisyui')],
-}
+  plugins: [require("daisyui")],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
+  },
+};
