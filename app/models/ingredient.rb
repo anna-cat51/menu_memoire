e0a593b11bc6a823bd937a228b35883954517fe4
AnnaDoi <willow6510@yahoo.co.jp>
2023-11-22 14:46:39 +0900

    add: reperoireとingredientの中間テーブル作成

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ApplicationRecord
+  has_many :repertoire_ingredients, dependent: :destroy
+  has_many :repertoire, through: :repertoire_ingredients
+  accepts_nested_attributes_for :repertoire_ingredients
+
+  validates :name, presence: true, uniqueness: true
 end
