c4624a9e1ca2c8c25d1fb74b5e9a10d5103ea603
AnnaDoi <willow6510@yahoo.co.jp>
2023-11-22 14:40:27 +0900

    add: ingdientテーブル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+
+  User.create!(
+    email: Faker::Internet.email,
+  encrypted_password: Devise.friendly_token[0, 20], # ダミーのパスワードを生成
+  provider: 'sns_provider', # 適切なSNSのプロバイダを指定
+  uid: 'sns_uid', # SNSのUIDを指定
+  name: Faker::Name.first_name
+)
+
+20.times do |repertoire|
+	Board.create(
+		user: User.offset(rand(User.count)).first,
+		name: "レシピ名#{repertoire}",
+		recipe_url: "レシピのURL#{repertoire}"
+	)
+end
