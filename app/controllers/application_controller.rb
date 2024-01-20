7483082f210fb2f24685f7c70b97b159576e27c9
AnnaDoi <willow6510@yahoo.co.jp>
2024-01-20 07:40:42 +0000

    RecordNotFound時トップページにリダイレクトする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
 
   def require_login
     redirect_to login_path, warning: 'ログインしてください' if current_user.blank?
   end
-
+  # エラー時の処理を定義するプライベートメソッド
+  def record_not_found
+    # ユーザーにエラーメッセージを表示する、または別のページにリダイレクトする
+    flash[:alert] = 'このページは存在しないか、アクセスできません'
+    redirect_to root_path # 例：ホームページにリダイレクト
+  end
 end
