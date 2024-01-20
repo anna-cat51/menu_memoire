05b9eed36dbfb53363f9dc79d88a17756e2c9557
AnnaDoi <willow6510@yahoo.co.jp>
2024-01-21 03:42:17 +0900

    コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root "tops#index"
 
   devise_for :users, controllers: {
@@ -17,10 +16,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   post '/callback', to: 'linebot#callback'
 
-=======
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
->>>>>>> 0348fbfde0827f1a432af27f62fe22ff6c4c7113
 end
