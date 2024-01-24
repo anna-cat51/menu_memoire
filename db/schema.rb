7330d52288da6cae06f581fc2a9cfd5d56e09218
AnnaDoi <willow6510@yahoo.co.jp>
2024-01-24 17:10:32 +0000

    rubocop修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_01_24_163313) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -21,43 +21,43 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_24_163313) do
     t.index ["name"], name: "index_ingredients_on_name", unique: true
   end
 
-  create_table 'repertoire_ingredients', force: :cascade do |t|
-    t.bigint 'repertoire_id', null: false
-    t.bigint 'ingredient_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['ingredient_id'], name: 'index_repertoire_ingredients_on_ingredient_id'
-    t.index %w[repertoire_id ingredient_id], name: 'unique_repertoire_ingredient_index', unique: true
-    t.index ['repertoire_id'], name: 'index_repertoire_ingredients_on_repertoire_id'
+  create_table "repertoire_ingredients", force: :cascade do |t|
+    t.bigint "repertoire_id", null: false
+    t.bigint "ingredient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_repertoire_ingredients_on_ingredient_id"
+    t.index ["repertoire_id", "ingredient_id"], name: "unique_repertoire_ingredient_index", unique: true
+    t.index ["repertoire_id"], name: "index_repertoire_ingredients_on_repertoire_id"
   end
 
-  create_table 'repertoires', force: :cascade do |t|
-    t.string 'name'
-    t.string 'recipe_url'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'repertoire_image'
-    t.index ['user_id'], name: 'index_repertoires_on_user_id'
+  create_table "repertoires", force: :cascade do |t|
+    t.string "name"
+    t.string "recipe_url"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "repertoire_image"
+    t.index ["user_id"], name: "index_repertoires_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'provider'
-    t.string 'uid'
-    t.string 'name'
-    t.integer 'status', default: 0, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.integer "status", default: 0, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'repertoire_ingredients', 'ingredients'
-  add_foreign_key 'repertoire_ingredients', 'repertoires'
-  add_foreign_key 'repertoires', 'users'
+  add_foreign_key "repertoire_ingredients", "ingredients"
+  add_foreign_key "repertoire_ingredients", "repertoires"
+  add_foreign_key "repertoires", "users"
 end
