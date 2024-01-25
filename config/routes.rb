9efae2453f42cc10ea5b3904c18bd3f54dbc406c
AnnaDoi <willow6510@yahoo.co.jp>
2024-01-25 01:43:59 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,15 @@ Rails.application.routes.draw do
   omniauth_callbacks: "omniauth_callbacks",
   sessions: "sessions"
 }
+
+  resources :repertoires do
+    collection do
+      post :scrape
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-
-  get 'privacy_policy', to: 'tops#privacy_policy'
-  get 'terms_of_use', to: 'tops#terms_of_use'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/callback', to: 'linebot#callback'
 
 end
