4a31476acc4dee6283de5a09f0aaabd06e67b30b
AnnaDoi <willow6510@yahoo.co.jp>
2024-01-31 08:19:21 +0000

    SearchContens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_28_171329) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_31_081637) do
   create_table "ingredients", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -38,7 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_28_171329) do
     t.index ["user_id"], name: "index_repertoires_on_user_id"
   end
 
-  create_table "search_contents", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "search_contents", charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.bigint "repertoire_id"
     t.bigint "ingredient_id"
     t.string "origin_repertoire_name"
