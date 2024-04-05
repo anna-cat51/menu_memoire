d38f8b444d27ee658db62cb62b1d5aff95910fb2
AnnaDoi <willow6510@yahoo.co.jp>
2024-04-05 06:36:10 +0000

    不必要なコメントアウトの削除

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   post '/callback', to: 'linebot#callback'
 
 end
