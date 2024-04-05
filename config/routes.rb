ee1a51540e80b384895de6ef7f878e0869a091b2
AnnaDoi <willow6510@yahoo.co.jp>
2024-04-05 06:41:52 +0000

    プライバシーポリシー、利用規約追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :searches, only: [:index]
   root "tops#index"
+  get '/terms_of_use', to: 'tops#terms_of_use'
+  get '/privacy_policy', to: 'tops#privacy_policy'
   devise_for :users, controllers: {
   omniauth_callbacks: "omniauth_callbacks",
   sessions: "sessions"
